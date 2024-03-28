74ec0dfd21acbd82ce53d6346426187d9ef305cf
Amin7775 <amin.dpi24@gmail.com>
2024-03-28 14:40:13 +0600

    Completed pages to read page and added chart from rechart , imported data from localstorage

--- src/components/PagesToRead/PagesToRead.jsx
+++ src/components/PagesToRead/PagesToRead.jsx
@@ -1,9 +1,28 @@
+import { getReadData } from "../../utilities/LocalStorage/localStorage";
+import Chart from "./Chart";
 
 const PagesToRead = () => {
+
+    const readData = getReadData()
+    
+
     return (
-        <div>
-            This Is PagesToRead
+      <div>
+        <div className="bg-[#1313130D] lg:rounded-2xl my-10">
+          <h1 className="font-bold text-3xl text-center p-10 ">
+            Pages To Read
+          </h1>
         </div>
+        {readData.length !== 0 ? (
+          <Chart data={readData}></Chart>
+        ) : (
+          <div className="flex h-full min-h-[50vh] items-center justify-center">
+            <h1 className="text-2xl font-semibold px-10 text-center">
+              Please add books on your read list before visiting this page
+            </h1>
+          </div>
+        )}
+      </div>
     );
 };
 
