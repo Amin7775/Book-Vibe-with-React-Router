a8f4b79b54eae95cb97f635751e4e182932490ec
Amin7775 <amin.dpi24@gmail.com>
2024-03-28 13:57:52 +0600

    Made viewDetails page responsive

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -66,11 +66,11 @@ const BookDetails = () => {
   };
 
   return (
-    <div className="flex my-11 gap-16">
-      <div className="w-5/12 bg-[#1313130D] p-16 max-h-[85vh] overflow-hidden rounded-md">
+    <div className="flex flex-col lg:flex-row my-6 lg:my-11 gap-10 lg:gap-16">
+      <div className="w-full lg:w-5/12 bg-[#1313130D] p-16 max-h-[85vh] overflow-hidden lg:rounded-md">
         <img className="h-full mx-auto" src={image} alt="" />
       </div>
-      <div className="flex-1">
+      <div className="flex-1 px-5 lg:px-0">
         <h1 className="text-4xl font-bold mb-5">{bookName}</h1>
         <h3 className="text-[#131313CC] text-xl font-medium mb-5">
           By : {author}
@@ -78,18 +78,18 @@ const BookDetails = () => {
         <div className="border-b border-[#13131326] mb-4"></div>
         <p className="text-[#131313CC] text-xl font-medium mb-3">{category}</p>
         <div className="border-b border-[#13131326] mb-5"></div>
-        <p className="text-[#131313B3] mb-10">
+        <p className="text-[#131313B3] mb-7 lg:mb-10">
           <span className="font-bold text-[#131313]">Review : </span>
           {review}
         </p>
-        <div className="flex items-center gap-2 mb-6">
+        <div className="flex items-center gap-2 mb-5 lg:mb-6">
           <p>Tag</p>
           {tags.map((tag, id) => (
             <Tags key={id} tag={tag}></Tags>
           ))}
         </div>
         <div className="border-b border-[#13131326] mb-6"></div>
-        <div className="w-[40%] space-y-3">
+        <div className=" w-full lg:w-[40%] space-y-3">
           <div className="flex justify-between">
             <p className="text-[#131313B3]">Number of Pages :</p>
             <p className="font-semibold text-[#131313]">{totalPages}</p>
@@ -108,7 +108,7 @@ const BookDetails = () => {
           </div>
         </div>
         {/* btn */}
-        <div className="flex gap-4 mt-7">
+        <div className="flex justify-center lg:justify-start gap-4 my-7">
           <button
             onClick={handleRead}
             className="btn bg-transparent border border-[#1313134D] text-lg font-semibold px-6 py-2 h-full"
